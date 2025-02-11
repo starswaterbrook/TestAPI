eff36113e1721e674c56b22c2c48cb51a44f28b0
Maciej Kmieć <mackmi1@st.amu.edu.pl>
2025-02-11 17:50:53 +0100

    chore: better logging

--- src/test/java/org.example/base/TestLogger.java
+++ src/test/java/org.example/base/TestLogger.java
@@ -2,29 +2,37 @@ package org.example.base;
 
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+import java.util.Arrays;
 
 
 public class TestLogger implements ITestListener {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private String clampParameter(Object param) {
+        String fullString = String.valueOf(param);
+        return fullString.length() > 40 ? fullString.substring(0, 37) + "...]" : fullString;
+    }
 
-    private String timestamp() {
-        return "[" + LocalDateTime.now().format(formatter) + "]";
+    private String formatParameters(ITestResult result) {
+        Object[] parameters = result.getParameters();
+        if (parameters.length == 0) {
+            return "";
+        }
+        String fullParamString = Arrays.toString(parameters);
+        return " | Parameters: " + clampParameter(fullParamString);
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        System.out.println(timestamp() + " PASS: " + result.getMethod().getMethodName());
+        System.out.println("[PASS] " + result.getMethod().getMethodName() + formatParameters(result));
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        System.out.println((timestamp() + " FAILURE: " + result.getMethod().getMethodName()));
+        System.out.println("[FAILURE] " + result.getMethod().getMethodName() + formatParameters(result));
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-        System.out.println((timestamp() + " SKIPPED: " + result.getMethod().getMethodName()));
+        System.out.println("[SKIPPED] " + result.getMethod().getMethodName() + formatParameters(result));
     }
 }
